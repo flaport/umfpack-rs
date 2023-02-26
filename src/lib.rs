a7d93086cfe6640bb1d4d7c942a2550d8445967c
flaport <floris.laporte@gmail.com>
2023-02-25 20:30:51 -0800

    start working on rust conversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,28 @@
+use libc::strlen;
+use std::slice;
+
 mod c {
     extern "C" {
-        pub fn example() -> i32;
+        pub fn example();
+        pub fn SuiteSparse_BLAS_library() -> *const i8;
+        pub fn solve(n: i32, Ap: *const i32, Ai: *const i32, Ax: *const f64, b: *const f64);
     }
 }
 
-pub fn example() -> i32 {
-    unsafe {
-        c::example()
-    }
+pub fn example() {
+    unsafe { c::example() }
+}
+
+#[allow(non_snake_case)]
+pub fn solve(n: i32, Ap: &[i32], Ai: &[i32], Ax: &[f64], b: &[f64]) {
+    unsafe { c::solve(n, Ap.as_ptr(), Ai.as_ptr(), Ax.as_ptr(), b.as_ptr()) }
+}
+
+#[allow(non_snake_case)]
+pub fn SuiteSparse_BLAS_library() -> String {
+    let ptr = unsafe { c::SuiteSparse_BLAS_library() };
+    let len = unsafe { strlen(ptr) };
+    let vec = unsafe { slice::from_raw_parts(ptr, len) };
+    let string = String::from_utf8(vec.iter().map(|i| *i as u8).collect()).unwrap();
+    return string;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
-use umfpack::example;
+use umfpack::{example, solve, SuiteSparse_BLAS_library};
+
+#[allow(non_snake_case)]
 fn main() {
-    let status = example();
-    println!("{status}");
+    let blas_version = SuiteSparse_BLAS_library();
+    println!("{blas_version}");
+
+    example();
+    println!("---");
+
+    let n = 5;
+    let Ap = &[0, 2, 5, 9, 10, 12];
+    let Ai = &[0, 1, 0, 2, 4, 1, 2, 3, 4, 2, 1, 4];
+    let Ax = &[2.0, 3.0, 3.0, -1.0, 4.0, 4.0, -3.0, 1.0, 2.0, 2.0, 6.0, 1.0];
+    let b = &[8.0, 45.0, -3.0, 3.0, 19.0];
+    solve(n, Ap, Ai, Ax, b);
 }
